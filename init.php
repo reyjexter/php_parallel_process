fa5d04d71c5901b001061aba2dd67f7246cf6b63
Rey Bumalay <rey@limechalk.com>
2014-02-26 08:21:50 +0800

    calculate usage with cpu core in mind

--- init.php
+++ init.php
@@ -2,10 +2,25 @@
 
 $system_load = sys_getloadavg();
 $load_last_one_minute = $system_load[0];
+$os = PHP_OS;
+$cpu_cores = 1;
 
-echo "server load: $load_last_one_minute\n";
+// get the cpu cores per platform
+//
+if($os === 'Darwin') {
+	// this is a mac
+	$cpu_cores = exec("sysctl hw.ncpu | awk '{print $2}'");
+}
+else {
+	// linux
+	$cpu_cores = exec("grep -c processor /proc/cpuinfo");
+}
 
-if($load_last_one_minute > 80) {
+$server_load_percentage = round(($load_last_one_minute / $cpu_cores) * 100, 2);
+echo "server load ($cpu_cores cores): $server_load_percentage %\n";
+
+
+if($server_load_percentage > 80) {
 	echo "server is too busy...\n";
 	return;
 }
